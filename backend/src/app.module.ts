1bf40ba1bc8dc9fc25c3625c7f0a53fc844b0087
Liri Rexha <lirie.rexhaa@gmail.com>
2026-07-12 20:32:31 +0200

    chor: add prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, OrdersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
