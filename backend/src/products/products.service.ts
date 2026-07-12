43cd0804c71f5a97382622466d8744c72d04b5a3
Liri Rexha <lirie.rexhaa@gmail.com>
2026-07-12 20:55:16 +0200

    add product endpoints

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      return await this.prisma.product.create({
+        data: createProductDto,
+      });
+    } catch {
+      throw new Error("can't create product"); //P2002 code for unique constraint
+    }
+  }
+
+  async findAll() {
+    return this.prisma.product.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    try {
+      return await this.prisma.product.update({
+        where: { id },
+        data: updateProductDto,
+      });
+    } catch {
+      throw new Error("Can't update product");
+    }
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.prisma.product.delete({
+      where: { id },
+    });
+  }
+}
